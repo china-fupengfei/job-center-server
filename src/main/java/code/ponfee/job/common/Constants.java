f1d716b92f2ac2cf8f8fb3748bc211352f645e37
付鹏飞[01367825] <fupengfei1@sf.express.com>
2017-07-22 14:47:47 +0800

    优化

--- src/main/java/code/ponfee/job/common/Constants.java
+++ src/main/java/code/ponfee/job/common/Constants.java
@@ -1,14 +1,40 @@
 package code.ponfee.job.common;
 
+import java.util.Map;
+import java.util.Set;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+
+import code.ponfee.commons.reflect.ClassUtils;
+import code.ponfee.commons.resource.ResourcesScanner;
 import code.ponfee.commons.util.Networks;
+import code.ponfee.job.sched.handler.JobHandlerMeta;
 
 /**
  * 常量类
  * @author fupf
  */
+@SuppressWarnings("unchecked")
 public final class Constants {
     public static final String CACHE_KEY_PREFIX = "sched:"; // cache key prefix of job center
     public static final String IP_ADDRESS = Networks.getSiteIp();
     public static final Integer PLACEHOLDER = Integer.MIN_VALUE;
     public static final int MAX_ERROR_LENGTH = 8000;
+
+    public static final Map<String, String> JOB_HANDLER_CONFIGS;
+    static {
+        Set<Class<?>> classes = new ResourcesScanner("code.ponfee.job").scan4class(new Class[] { JobHandlerMeta.class });
+        Builder<String, String> builder = ImmutableMap.builder();
+        for (Class<?> clazz : classes) {
+            builder.put(ClassUtils.getClassName(clazz), clazz.getAnnotation(JobHandlerMeta.class).value());
+        }
+        JOB_HANDLER_CONFIGS = builder.build();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(JOB_HANDLER_CONFIGS);
+        JOB_HANDLER_CONFIGS.put("a", "b");
+    }
+
 }
--- src/main/java/code/ponfee/job/common/SpringStartupListener.java
+++ src/main/java/code/ponfee/job/common/SpringStartupListener.java
@@ -1,16 +1,8 @@
 package code.ponfee.job.common;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
-
-import code.ponfee.job.sched.handler.JobHandlerMeta;
-import code.ponfee.commons.reflect.ClassUtils;
-import code.ponfee.commons.resource.ResourcesScanner;
 
 /**
  * spring容器启动完成后执行
@@ -18,32 +10,15 @@ import code.ponfee.commons.resource.ResourcesScanner;
  */
 @Component
 public class SpringStartupListener implements ApplicationListener<ContextRefreshedEvent> {
-    private static final Map<String, String> JOB_HANDLER_CONFIGS = new HashMap<>();
 
     /**
      * spring初始化完成后执行一次
      */
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
-        if (event.getApplicationContext().getParent() != null) return;
-
-        loadJobHandlers();
-    }
-
-    @SuppressWarnings("unchecked")
-    private void loadJobHandlers() {
-        ResourcesScanner scanner = new ResourcesScanner("code.ponfee.job");
-        Set<Class<?>> classes = scanner.scan4class(new Class[]{JobHandlerMeta.class});
-        for (Class<?> clazz : classes) {
-            String name = clazz.getAnnotation(JobHandlerMeta.class).value();
-            JOB_HANDLER_CONFIGS.put(ClassUtils.getClassName(clazz), name);
+        if (event.getApplicationContext().getParent() == null) {
+            // do something for application init
         }
-    }
-
-    public static Map<String, String> listJobHandlers() {
-        Map<String, String> result = new HashMap<>();
-        result.putAll(JOB_HANDLER_CONFIGS);
-        return result;
     }
 
 }
